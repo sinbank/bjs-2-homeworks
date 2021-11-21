b523030328c9fa05097452d2d5997b04508f2bb2
sinbank <94779411+sinbank@users.noreply.github.com>
2021-11-21 12:22:06 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,43 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let x1;
+  let x2;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [(-b + Math.sqrt(d))/(2*a)];
+  } else {
+    x1 = (-b + Math.sqrt(d) )/(2*a);
+    x2 = (-b - Math.sqrt(d) )/(2*a) ;
+    arr.push(x1, x2);
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let per = parseInt(percent) / 100 / 12; //процентная ставка
+    let con = parseInt(contribution); 	  //начальный взнос	
+    let am = parseInt(amount);  			  //общая стоимость	
+  	if (isNaN(per) || per < 0) {
+   		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+   	} else if (isNaN(con) || con < 0) {
+   		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+   	} else if (isNaN(am) || am < 0) {
+   		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+   	} else {
+			
+  		let today = new Date();
+  		if (today.getFullYear() > date.getFullYear()) {
+			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
+		} else {
+			let s = am - con;						  //тело кредита
+  			let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); //срок кредита в месяцах
+  			let pay = s*(per+per/(((1+per)**n)-1));			//ежемесячная оплата
+  			let totalAmount = (pay * n).toFixed(2);
+  			console.log(totalAmount);
+ 			return +totalAmount;
+ 		}	
+    }
+  return +totalAmount;
 }
