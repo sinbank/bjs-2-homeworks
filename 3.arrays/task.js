44967f5631a40f7b353eea7617b4bf386dd2acf7
sinbank <94779411+sinbank@users.noreply.github.com>
2021-11-28 14:09:04 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,11 +2,9 @@ alert(`Задача №1: `);
 const compareArrays = (a, b) => a.length === b.length && a.every((n, i) => n === b[i]);
 console.log (compareArrays([8, 9], [6]));
 alert(`Задача №2: `);
-//function advancedFilter(arr) {
-//let result = arr.filter((n) => n > 0).filter((n) => !(n % 3)).map((n) => n * 10);
 let arr = [];
 function advancedFilter(arr) {
-let result = arr.filter((n) => n > 0).filter((n) => !(n % 3)).map((n) => n * 10);
-return result;
+ let result = arr.filter((n) => n > 0).filter((n) => !(n % 3)).map((n) => n * 10);
+ return result;
 }
 console.log (advancedFilter ([-1,6,-9,3]));
