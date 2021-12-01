cda7f2af2e1ad9a8314afdb12938005251758e56
sinbank <94779411+sinbank@users.noreply.github.com>
2021-12-01 17:48:13 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,5 +1,3 @@
-//Задача 1
-
 class PrintEditionItem {
     constructor (name, releaseDate, pagesCount) {
         this.name = name;
@@ -30,17 +28,10 @@ class PrintEditionItem {
     }
 }
 
-const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
-
-console.log(sherlock.releaseDate); //2019
-console.log(sherlock.state); //100
-sherlock.fix();
-console.log(sherlock.state); //100
-
 class Magazine extends PrintEditionItem {
     constructor (name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
-        this.type = "Magazine";
+        this.type = "magazine";
     }
 }
 
@@ -72,26 +63,16 @@ class DetectiveBook extends Book {
         this.type = "detective";
     }
 }
-    
-const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+   
 
-console.log(picknick.author); //"Аркадий и Борис Стругацкие"
-picknick.state = 10;
-console.log(picknick.state); //10
-picknick.fix();
-console.log(picknick.state); //15
-
-//Задача 2
-
-class Library extends PrintEditionItem { 
-    constructor (name, releaseDate, pagesCount) {
-        super(releaseDate, pagesCount);
+class Library { 
+    constructor (name) {
         this.name = name;
         this.books = [];
     }
 
     addBook(book) {
-        if (this.state > 30) {
+        if (book.state > 30) {
             this.books.push(book)
         }
     }
@@ -117,8 +98,22 @@ class Library extends PrintEditionItem {
 
 const library = new Library("Библиотека имени Ленина");
 
-library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
-library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(
+  new DetectiveBook(
+    "Артур Конан Дойл",
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  )
+);
+library.addBook(
+  new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  )
+);
 library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
 library.addBook(new Magazine("Мурзилка", 1924, 60));
 
